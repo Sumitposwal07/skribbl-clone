8564c5312c3edee39de0bb5a27cf4cfb6d45d13c
sumit <poswalsumit26@gmail.com>
2026-06-14 19:49:41 +0530

    Prepare backend for production

--- server/src/server.js
+++ server/src/server.js
@@ -4,23 +4,28 @@ const { Server } = require("socket.io");
 const cors = require("cors");
 
 const roomSocket = require("./sockets/roomSocket");
+require("dotenv").config();
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: process.env.CLIENT_URL,
+  credentials: true
+}));
 
 const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "*",
-        methods: ["GET", "POST"]
+         origin: process.env.CLIENT_URL,
+         credentials: true,
+         methods: ["GET", "POST"]
     }
 });
 
 roomSocket(io);
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
